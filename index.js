cd26e8da35d75094338862de9ab5f62b3356ae85
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2025-12-19 20:37:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -491,8 +491,7 @@ app.get('/api/random/aigirls', async (req, res) => {
     res.json(result);
 });
 
-if (require.main === module) {
-    app.listen(PORT, () => {
+app.listen(PORT, () => {
         console.log(`Server berjalan di port ${PORT}`);
     });
 }
