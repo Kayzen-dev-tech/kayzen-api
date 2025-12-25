dbf5c5562317a4eaaeb0bef78fb60ad2a1c1616d
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2025-12-25 09:38:06 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const cosplay=document.getElementById("cosplay")
-for(let i=1;i<=15;i++){let img=document.createElement("img");img.src=`images/slide${i}.jpg`;cosplay.appendChild(img)}
+for(let i=1;i<=15;i++){let img=document.createElement("img");img.src=`slide${i}.jpg`;cosplay.appendChild(img)}
 
 const bini=document.getElementById("bini")
-for(let i=1;i<=5;i++){let img=document.createElement("img");img.src=`images/bini${i}.jpg`;bini.appendChild(img)}
+for(let i=1;i<=5;i++){let img=document.createElement("img");img.src=`bini${i}.jpg`;bini.appendChild(img)}
