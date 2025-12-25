dabc95d7898a43bf4d2ac3cf1f4a2cb702443a26
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2025-12-25 10:17:10 +0700

    Delete api directory

--- api/tikwm.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export default async function(req, res) {
-  const url = new URL(req.url, "http://x")
-  const video = url.searchParams.get("url")
-  const r = await axios.post("https://tikwm.com/api/", { url: video })
-  res.json(r.data)
-}
