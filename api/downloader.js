db9bcacab4babeedba2e93cfe02b615e07f58f95
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2025-12-29 22:56:39 +0700

    Update downloader.js

--- api/downloader.js
+++ api/downloader.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const downloader = {
     tiktok: async (url) => {
-        const res = await axios.get(`https://api.tiklydown.eu.org/api/download?url=${url}`);
+        const res = await axios.get(`https://api.deline.web.id/downloader/tiktok?url=${url}`);
         return res.data;
     },
     youtube: async (url, type) => {
