c8d48763892292afa3ac28f0ef04e36a66354f1d
K ɪɴꜰɪɴɪᴛʏ <amnakhoirulzamzam10@gmail.com>
2025-12-29 22:43:26 +0700

    Create downloader.js

---
+++ api/downloader.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const downloader = {
+    tiktok: async (url) => {
+        const res = await axios.get(`https://api.tiklydown.eu.org/api/download?url=${url}`);
+        return res.data;
+    },
+    youtube: async (url, type) => {
+        const res = await axios.get(`https://api.y2mate.tools/v1/download?url=${url}&type=${type}`);
+        return res.data;
+    },
+    instagram: async (url) => {
+        const res = await axios.get(`https://api.indown.io/api/get?url=${url}`);
+        return res.data;
+    },
+    pinSearch: async (query) => {
+        const res = await axios.get(`https://www.pinterest.com/resource/BaseSearchResource/get/?data={"options":{"query":"${query}","page_size":5}}`);
+        return res.data.resource_response.data.results.map(v => v.images.orig.url);
+    },
+    pinDown: async (url) => {
+        return { status: true, url: url.replace('/pin/', '/offscreen/') };
+    },
+    twitter: async (url) => {
+        const res = await axios.get(`https://twitsave.com/api/info?url=${url}`);
+        return res.data;
+    }
+};
+
+module.exports = downloader;
