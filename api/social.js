6cd157df92ce6e9d1078e0fba046dd7dd8c711b4
Kayzen Izumi <amnakhoirulzamzam10@gmail.com>
2026-01-19 06:40:02 +0700

    Delete api directory

--- api/social.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    tiktok: "https://tiktok.com/@sczkayzen",
-    instagram: "https://instagram.com/kayzenfry",
-    youtube: "https://youtube.com/@kayzenfry",
-    github: "https://github.com/Kayzen-dev-tech"
-  });
-}
--- api/time.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const now = new Date();
-  res.json({
-    time: now.toLocaleTimeString("id-ID", { hour12: false }),
-    date: now.toLocaleDateString("id-ID")
-  });
-}
